4d0beb1cf4adac56fade0bdf54a8e436a35827fa
Michał Postek <michalpostek@MacBook-Air-Micha.local>
2020-08-11 23:53:11 +0200

    add background color change on scroll event to hero section,remove/add thanks to IntersectionObersver

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,5 +1,8 @@
-const heroContainer = document.querySelector('[data-hero-images]');
+const heroImagesContainer = document.querySelector('[data-hero-images]');
 const heroModals = [...document.querySelectorAll('[data-hero-modal]')];
+const backgroundLayer = document.querySelector('[data-change-bg');
+const heroHeaderText = document.querySelector('[data-hero-header');
+const heroContainer = document.querySelector('[data-hero-container');
 
 const hideModals = () =>
   heroModals.forEach(modal => modal.classList.remove('is-open'));
@@ -12,4 +15,24 @@ const toggleHeroModal = ({ target }) => {
   }
 };
 
-heroContainer.addEventListener('click', toggleHeroModal);
+const observerFn = entries => {
+  let heroContainerEntry = entries[0];
+  heroContainerEntry.isIntersecting
+    ? window.addEventListener('scroll', changeBackground)
+    : window.removeEventListener('scroll', changeBackground);
+};
+
+let observer = new IntersectionObserver(observerFn);
+observer.observe(heroContainer);
+
+const changeBackground = () => {
+  const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  const opacityPromile = 1400 / 1000000;
+  const finalOpacity = scrollTop * opacityPromile;
+  backgroundLayer.style.opacity = 1 - finalOpacity;
+  finalOpacity < 0.5
+    ? (heroHeaderText.style.color = '#fff')
+    : (heroHeaderText.style.color = '#000');
+};
+
+heroImagesContainer.addEventListener('click', toggleHeroModal);
